3ae3da279e4ec6b0a7f07af1778ced2c1638e592
kate <katya150491@gmail.com>
2022-12-13 13:22:38 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+//alert('Hello');
+
+//const result = confirm("Are tou here?");
+//console.log(result);
+
+//const answer = +prompt('вам есть 18?', '18');
+//console.log(answer + 5);
+
+
+//console.log('arr' + "- object");
+//console.log(4 + +'5');
+
+
+console.log(2+2*2 != 8);
